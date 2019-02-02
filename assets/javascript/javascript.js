3aa12d47e57755d9c77f001ae9698180ef04fea9
sthmpsn <sthmpsn@gmail.com>
2019-02-02 11:35:12 -0600

    Merge with bill and steve front end - initial attempt

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+// BEGIN STAR RATING RELATED
+    $(document).on("click", ".star-ratings > .fa-star", function(){
+        // Clear any existing stars that were previously colored since a rating change is desired
+        $(".star-ratings > .fa-star").removeClass("checked");
+
+        // Get the Clicked Object
+            var starNum = $(this)
+            console.log("Star Object: " + starNum);
+        // Get the start rating (1,2,3,4 or 5)
+            var starRating = starNum.attr("rating")  
+            console.log("RATING: "+ starRating);
+        // For loop counting down to 1 that runs starting from the star-num clicked
+            for (var i=starRating; i > 0; i--){ 
+                $('#rating'+i).addClass("checked");    
+                console.log("Color Filled Star " +i+ " with gold")
+            }
+    });
+// END STAR RATING RELATED
+
+
+
+
+
+
+
+
+
+
+});
